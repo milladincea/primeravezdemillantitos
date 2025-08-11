785c12a16c8e7af33cd00ae5df59c34112b6db4d
Millaray Dinamarca C. <millaray.dinamarca@ug.uchile.cl>
2025-08-11 17:20:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function setup() {
 function draw() {
   d = random (10,60);
   colorMode(HSB,360,100,100);
-  fill(random(0,360),100,100,random(55,200));
+  fill(random(0,360),100,100);
   ellipse(mouseX,mouseY,d,d);
 }
 
